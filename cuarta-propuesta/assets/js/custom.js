1ed8a76243bb5f43f98cec61e3ebe8d32dbaac76
Paty Arakawa <paty.arakawa@gmail.com>
2016-05-23 22:18:57 -0500

    slide index comentado, falta modal de curso

--- cuarta-propuesta/assets/js/custom.js
+++ cuarta-propuesta/assets/js/custom.js
@@ -1,1 +1,11 @@
 /* Write here your custom javascript codes */
+
+/*$(document).ready(function(){
+  $('.sliderCursos').bxSlider({
+    slideWidth: 350,
+    minSlides: 1,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 0
+  });
+});*/
